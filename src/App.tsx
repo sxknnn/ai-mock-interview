c6542b8d5aaf3c79f72b2e288107abf92936d36f
sxknnn <sxk4940@mavs.uta.edu>
2025-02-15 18:48:19 -0600

    changes : project setup completed

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+
+const App = () => {
+  return (
+    <div className='text-blue-500'>
+      <Button variant={"outline"} >Click me</Button>
+      </div>
+  );
+};
+
+export default App;
