7eb7d94ce8369a75d502f073d49ef1fbf6069602
sxknnn <sxk4940@mavs.uta.edu>
2025-04-06 10:11:57 -0500

    firebase added

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,11 +1,13 @@
 import Footer from "@/components/footer"
 import Header from "@/components/header"
+import AuthHandler from "@/handlers/auth-handler"
 import { Outlet } from "react-router-dom"
 
 
 export const PublicLayout = () => {
     return (
         <div className="w-full">
+            <AuthHandler/>
             <Header/>
 
             <Outlet />
